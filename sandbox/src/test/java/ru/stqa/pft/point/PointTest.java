77d2de2cfb153f1ecda9a646da1e2331ae8f75b5
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-02-14 07:54:54 +0300

    Добавлены тесты для Point

--- sandbox/src/test/java/ru/stqa/pft/point/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/point/PointTest.java
@@ -6,9 +6,21 @@ import org.testng.annotations.Test;
 
 public class PointTest {
   @Test
-  public void testPoint(){
+  public void testPoint1(){
     Point p1 = new Point(10,-2);
     Point p2 = new Point(20, 10);
     Assert.assertEquals(p1.distance(p2), 15.620499351813308);
+  }
+  @Test
+  public void testPoint2(){
+    Point p1 = new Point(-15,2);
+    Point p2 = new Point(-15, 2);
+    Assert.assertEquals(p1.distance(p2), 0);
+  }
+  @Test
+  public void testPoint3(){
+    Point p1 = new Point(0,0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 0);
   }
 }
