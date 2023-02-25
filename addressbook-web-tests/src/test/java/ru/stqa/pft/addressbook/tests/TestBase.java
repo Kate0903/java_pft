4d8d06546094c2d1d4878006ad1ab36c2aec90b9
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-02-25 13:14:20 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,21 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.BrowserType;
+
+
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
-  private WebDriver wd;
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  //private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
