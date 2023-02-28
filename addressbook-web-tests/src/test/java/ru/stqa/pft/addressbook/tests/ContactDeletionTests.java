5ff9d229c3e43ca5b4d53f07610f8958045ee5a1
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-02-28 09:08:47 +0300

    Внесены правки в предусловия удаления и модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -2,12 +2,17 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactDeletionTests extends TestBase{
   @Test
   public void testContactDeletion(){
     app.getNavigationHelper().gotoHomePage();
     if (!app.getContactHelper().isThereAContact()){
+      if (!app.getGroupHelper().isThereAGroup()){
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      }
+      app.getNavigationHelper().gotoAddContactPage();
       app.getContactHelper().createContact(new ContactData("kate", "kap", "89562", "Tokorevskaya","ghj@mail.ru","test1"));
     }
     app.getContactHelper().selectContact();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,12 +2,18 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase{
   @Test
   public void testContactModification(){
     app.getNavigationHelper().gotoHomePage();
     if (!app.getContactHelper().isThereAContact()){
+      app.getNavigationHelper().gotoGroupPage();
+      if (!app.getGroupHelper().isThereAGroup()){
+        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+      }
+      app.getNavigationHelper().gotoAddContactPage();
       app.getContactHelper().createContact(new ContactData("kate", "kap", "89562", "Tokorevskaya","ghj@mail.ru","test1"));
     }
     app.getContactHelper().editSelectedContact();
