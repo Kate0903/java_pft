cc6d349b64cfccc076d86119b8cc714617a26880
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-03-14 08:53:24 +0300

    Проверки параметров контакта перенесены в один класс ContactParametersTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactParametersTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactParametersTests.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase{
+public class ContactParametersTests extends TestBase{
   @Test
   public void testContactPhones(){
     app.goTo().homePage();
@@ -23,12 +23,35 @@ public class ContactPhoneTests extends TestBase{
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(), contact.getWorkPhone())
             .stream().filter((s) -> !s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactParametersTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
   public static String cleaned(String phone){
     return phone.replaceAll("\\s","").replaceAll("[-()]","");
+  }
+
+  @Test
+  public void testContactAddress(){
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+
+  }
+
+  @Test
+  public void testContactMails(){
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+
+  }
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(),contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> !s.equals(""))
+            .collect(Collectors.joining("\n"));
   }
 
 }
