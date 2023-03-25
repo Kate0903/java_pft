62957580ffc0b8bb69ad184375a263e9c2e0c6c6
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-03-25 19:27:07 +0300

    Добавлены условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,36 +1,63 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
-
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+  @Id
+  @Column (name = "id")
   private int id = 0;
   @Expose
+  @Column (name = "firstname")
   private String firstname;
   @Expose
+  @Column (name = "lastname")
   private String lastname;
   @Expose
+  @Column (name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
   @Expose
+  @Column (name = "address")
+  @Type(type = "text")
   private String address;
   @Expose
+  @Column (name = "email")
+  @Type(type = "text")
   private String email;
+  @Column (name = "work")
+  @Type(type = "text")
   private String workPhone;
+  @Column (name = "home")
+  @Type(type = "text")
   private String homePhone;
+  @Transient
+  private String group;
+  @Column (name = "email2")
+  @Type(type = "text")
   private String email2;
+  @Column (name = "email3")
+  @Type(type = "text")
   private String email3;
+  @Transient
   private String allEmails;
+  @Transient
   private String allPhones;
-  private File photo;
+  @Column (name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
