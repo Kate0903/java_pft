8273303fb4dad966b48abf8caf5c4270683d2c75
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-03-28 19:45:26 +0300

    Добавлены правки в тесты по добавлению и удалению групп у контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactIntoGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactIntoGroup.java
@@ -1,28 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.appmanager.ContactHelper;
-import ru.stqa.pft.addressbook.appmanager.GroupHelper;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class AddContactToGroup extends TestBase {
+public class AddContactIntoGroup extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -45,19 +37,26 @@ public class AddContactToGroup extends TestBase {
   }
 
   @Test
-  public void testAddContactToGroup() {
+  public void testAddContactIntoGroup() {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     ContactData modifiedContact = before.iterator().next();
     app.contact().selectContactById(modifiedContact.getId());
-    WebElement group = app.group().groupsOnAddressbookPage().iterator().next();
-    int groupId = Integer.parseInt(group.getAttribute("value"));
-    app.group().chooseGroup(groupId);
+    List<WebElement> groupsList = app.group().groupsOnAddressbookPage();
+    Groups groupsContact = app.db().groupsForContacts(modifiedContact.getId());
+    List<Integer> groupsIdList = groupsList.stream().mapToInt(g-> Integer.parseInt(g.getAttribute("value")))
+            .boxed().collect(Collectors.toList());
+    List<Integer> groupsIdContact = groupsContact.stream().mapToInt(g-> g.getId())
+            .boxed().collect(Collectors.toList());
+    List<Integer> others = groupsIdList.stream()
+            .filter(id->!groupsIdContact.contains(id))
+            .collect(Collectors.toList());
+    int otherIdGroup = others.iterator().next();
+    app.group().chooseGroup(otherIdGroup);
     app.contact().addContactIntoGroup();
     Groups groups = app.db().groupsForContacts(modifiedContact.getId());
     List<Integer> groupsIds = groups.stream().mapToInt(g->g.getId()).boxed().collect(Collectors.toList());
-    Assert.assertTrue(groupsIds.contains(groupId));
+    Assert.assertTrue(groupsIds.contains(otherIdGroup));
 
   }
-
 }
