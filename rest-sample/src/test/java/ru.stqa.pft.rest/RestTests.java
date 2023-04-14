089802256531f1591022304230c108606dc24368
Kate0903 <59795444+Kate0903@users.noreply.github.com>
2023-04-14 23:40:47 +0300

    REST: взаимодействие с BugifyСтраница

---
+++ rest-sample/src/test/java/ru.stqa.pft.rest/RestTests.java
@@ -0,0 +1,53 @@
+package ru.stqa.pft.rest;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class RestTests {
+  @Test
+    public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue().withSubject("Test issue1402Kate").withDescription("New test issue");
+   int issueId = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+  private Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json?pageSize=500"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("b31e382ca8445202e66b03aaf31508a3", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor().execute(Request.Post("http://bugify.stqa.ru/api/issues.json")
+                    .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                            new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+
+
+}
